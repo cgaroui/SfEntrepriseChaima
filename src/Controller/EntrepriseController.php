057cc4d88c93262445d500dfce35754040d52538
chaima garoui <chaimagaroui2@gmail.com>
2024-07-05 16:59:54 +0200

    ajout : vues et completer controller de entreprise et de l'entitée employer

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Entreprise;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EntrepriseController extends AbstractController
 {
     #[Route('/entreprise', name: 'app_entreprise')]
-    public function index(): Response
+
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        
+        $entreprises = $entityManager->getRepository(Entreprise::class)->findAll();
         return $this->render('entreprise/index.html.twig', [
-            'controller_name' => 'EntrepriseController',
+           'entreprises' => $entreprises
         ]);
+    }
+
+    //id ici la clé primaire de l'objet qu'on veut recuperer 
+    #[Route('/entreprise/(id)', name: 'show_entreprise')]
+    public function show(Entreprise $entreprise): Response
+    {
+        return $this->render('entreprise/show.html.twig', [
+            'entreprise' => $entreprise
+         ]);
     }
 }
--- src/Entity/Entreprise.php
+++ src/Entity/Entreprise.php
@@ -137,4 +137,9 @@ class Entreprise
 
         return $this;
     }
+
+    public function __toString(){
+
+        return $this->raisonSociale." (".$this->cp." ".$this->ville.")";
+    }
 }
